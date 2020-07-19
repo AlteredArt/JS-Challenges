db29aca68e5169b9973399b5f595173a93261255
Altered Art <cherokeedreemer@gmail.com>
2020-07-19 12:58:27 -0600

    start the excercises for objects

--- JS/objectives.js
+++ JS/objectives.js
@@ -43,7 +43,7 @@ const anotherSquare = new Square(1);
 // functions are objects
 function Circle(radius){
   this.radius = radius;
-  this.draw = fucntion(){
+  this.draw = function(){
     console.log('draw')
   }
 }
@@ -51,8 +51,6 @@ Circle.call({}, 1)
 Circle.apply({}, [1,2,3])
 
 const another = new Circle(1);
-
-
 // values vs reference types
 let x = 10;
 let y = x;
@@ -68,3 +66,41 @@ function increase(obj){
 }
 increase(obj);
 console.log(obj);
+// enumerating objects
+const triangle = {
+  radius: 1,
+  draw(){
+    console,log('triangle')
+  }
+};
+
+for (let key in triangle)
+  console.log(key, triangle[key])
+
+for( let key of Object.keys(circle))
+  console.log(key);
+
+for ( let entry of Object.entries(circle))
+  console.log(entry);
+
+if ("color" in triangle) console.log('yes');
+// Math objects
+Math.round(1.5)
+// string primative
+const message ="hi";
+// string object
+const anotherMessage = new String("hello");
+// escape notation
+const mess = 'This is my \'first message'
+// new line
+const messa = 'This is\n new line'
+// string primative
+const greeting = 'This is my' + 'First message'
+// Template Literals
+const message2 = `Hello my ${greeting} name`
+// date object
+const date1 = new Date()
+const now = new Date('May 11 2018 09:00')
+now.setFullYear(2017);
+
+// Excersizes
