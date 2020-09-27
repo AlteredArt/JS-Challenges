c80f10c8d7640f2a295c7344584c6f92d2367487
Altered Art <cherokeedreemer@gmail.com>
2020-09-27 02:53:16 -0600

    putting in work

--- JS/Arrays/range.js
+++ JS/Arrays/range.js
@@ -1,11 +1,12 @@
 // range
-function arrayFromRange (min, max){
-    const output = [];
-    for (let i = min; i <= max; i++)
-      output.push(i);
-      return output;
-  }
-  const arrayFrom = arrayFromRange(1,4);
-  console.log('range',arrayFrom);
-  
-  
+// takes a min an max arrgument
+function arrayFromRange(min, max) {
+  const output = [];
+  for (let i = min; i <= max; i++)
+    output.push(i);
+  return output;
+}
+const arrayFrom = arrayFromRange(2, 8);
+console.log('range:', arrayFrom);
+
+
