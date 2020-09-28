96ef6aa959318f8bd694587a09c59f1b65c0dbd2
Altered Art <cherokeedreemer@gmail.com>
2020-09-28 17:08:58 -0600

    finished course

--- JS/Data Structures/binaryTree.js
+++ JS/Data Structures/binaryTree.js
@@ -17,4 +17,32 @@ class Node {
             this.right = new Node(data)
         }
     }
+
+    contains(data) {
+        if (this.data === data) {
+            return this;
+        }
+        if (this.data < data && this.right) {
+            return this.right.contains(data);
+        } else if (this.data > data && this.left) {
+            return this.left.contains(data);
+        }
+        return null;
+    }
+
+    validate(node, min = null, max = null) {
+        if (max !== null && node.data > max) {
+            return false;
+        }
+        if (min !== null && node.data < min) {
+            return false;
+        }
+        if (node.left && !validate(node.left, min, node.data)) {
+            return false;
+        }
+        if (node.right && !validate(node.right, node.data, max)) {
+            return false;
+        }
+        return true;
+    }
 }
