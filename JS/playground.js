12579d1a0950645e2c9960ffc2ec62c86351d24d
Altered Art <cherokeedreemer@gmail.com>
2020-10-01 14:41:49 -0600

    work for the day

---
+++ JS/playground.js
@@ -0,0 +1,16 @@
+console.log('playground')
+// let array = ["may", "april", "august", "september"]
+
+// console.log(array.sort(function (a, b) { return a.length - b.length }))
+
+function sortByLength(arr) {
+    return arr.sort((a, b) => a.length - b.length)
+}
+
+
+
+
+sortByLength(["a", "ccc", "dddd", "bb"])
+sortByLength(["apple", "pie", "shortcake"])
+sortByLength(["may", "april", "september", "august"])
+sortByLength([])
